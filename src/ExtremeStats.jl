b1a23d3eb16e2c0a4a2f6375a00e2ebcb81c6b79
Fabian Gans <fgans@bgc-jena.mpg.de>
2014-11-27 14:54:54 +0100

    exported io functions

--- src/ExtremeStats.jl
+++ src/ExtremeStats.jl
@@ -3,7 +3,7 @@ module ExtremeStats
 include("anomalies.jl")
 include("features.jl")
 
-export get_anomalies, get_anomalies!, Extreme, load_X, label_Extremes, ExtremeList, Features, getFeatures, combineExtremes, sortby
+export get_anomalies, get_anomalies!, Extreme, load_X, label_Extremes, ExtremeList, Features, getFeatures, combineExtremes, sortby, writeExtremes, writeFeatures, writeTimeSeries
 import Images.label_components
 import NetCDF.ncread
 
