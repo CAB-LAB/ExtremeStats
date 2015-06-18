7dd9c41c0fbc64c7d01e7ab8e8e8b0536a1ca513
meggart <fabiangans@gmx.de>
2015-06-18 23:19:57 +0200

    modified read function

--- src/ExtremeStats.jl
+++ src/ExtremeStats.jl
@@ -33,13 +33,13 @@ function sortby!(el::ExtremeList,x::Vector;rev=false)
     el.extremes[:]=el.extremes[o]
 end
 
-function load_X(data_path,fileprefix,varname,years,lon_range,lat_range)
+function load_X(data_path,fileprefix,filepost,varname,years,lon_range,lat_range)
 
   N_years   = length(years)
   y=years[1]
-  lons=ncread(joinpath(data_path,"$(fileprefix)$(years[1]).nc"),"lon")
-  lats=ncread(joinpath(data_path,"$(fileprefix)$(years[1]).nc"),"lat")
-  time=ncread(joinpath(data_path,"$(fileprefix)$(years[1]).nc"),"time")
+  lons=ncread(joinpath(data_path,"$(fileprefix)$(years[1])$(filepost)"),"longitude")
+  lats=ncread(joinpath(data_path,"$(fileprefix)$(years[1])$(filepost)"),"latitude")
+  time=ncread(joinpath(data_path,"$(fileprefix)$(years[1])$(filepost)"),"time")
   println("lons: $(lons[1]) to $(lons[end])")
   println("lats: $(lats[1]) to $(lats[end])")
   NpY=length(time)
@@ -59,7 +59,7 @@ function load_X(data_path,fileprefix,varname,years,lon_range,lat_range)
   iyear=1
   for iyear in 1:N_years
       println("$iyear")
-      x[:,:,((iyear-1)*NpY+1):iyear*NpY]=ncread(joinpath(data_path,"$(fileprefix)$(years[iyear]).nc"),varname,[ilon_range[1],ilat_range[1],1],[nlon,nlat,-1]);
+      x[:,:,((iyear-1)*NpY+1):iyear*NpY]=ncread(joinpath(data_path,"$(fileprefix)$(years[iyear])$(filepost)"),varname,[ilon_range[1],ilat_range[1],1],[nlon,nlat,-1]);
   end
   return(x,lons,lats,ilon_range[1],ilat_range[1],nlon,nlat,NpY,N_years)
 end
@@ -95,7 +95,7 @@ end
 
 
 function nanquantile(xtest,q;useabs=false) 
-    x = similar(xtest)
+    x = Array(eltype(xtest),length(xtest))
     nanquantile!(xtest,q,x,useabs=useabs)
 end
 
